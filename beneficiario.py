3157999dfd213f9277190154af3fc849e6b15e17
Vitor Capdeville <vgsilva@mag.com.br>
2023-03-21 08:56:02 -0300

    inclusão do segurado e beneficiario

---
+++ beneficiario.py
@@ -0,0 +1,10 @@
+class Beneficiario:
+    def __init__(self, primeiro_nome, sobrenome, data_nascimento, cpf, rg, tipo, endereco, contato):
+        self.primeiro_nome = primeiro_nome
+        self.sobrenome = sobrenome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.rg = rg
+        self.tipo = tipo
+        self.endereco = endereco
+        self.contato = contato
---
+++ segurado.py
@@ -0,0 +1,10 @@
+class Segurado:
+    def __init__(self, primeiro_nome, sobrenome, data_nascimento, cpf, rg, endereco, contato, beneficiarios):
+        self.primeiro_nome = primeiro_nome
+        self.sobrenome = sobrenome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.rg = rg
+        self.endereco = endereco
+        self.contato = contato
+        self.beneficiarios = beneficiarios
