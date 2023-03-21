999ec20d51dd2072ecfcb9177efaaa34e0a64ec6
Vitor Capdeville <vgsilva@mag.com.br>
2023-03-21 08:56:22 -0300

    inclusao do endereco e do contato

---
+++ contato.py
@@ -0,0 +1,6 @@
+class Contato:
+    def __init__(self, celular, telefone_residencial, telefone_comercial, email):
+        self.celular = celular
+        self.telefone_residencial = telefone_residencial
+        self.telefone_comercial = telefone_comercial
+        self.email = email
---
+++ endereco.py
@@ -0,0 +1,8 @@
+class Endereco:
+    def __init__(self, rua, numero, complemento, cep, estado, cidade):
+        self.rua = rua
+        self.numero = numero
+        self.complemento = complemento
+        self.cep = cep
+        self.estado = estado
+        self.cidade = cidade
