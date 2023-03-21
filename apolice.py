8e9456a00b3e66488886e1d27b0f7c05959fb05a
Vitor Capdeville <vgsilva@mag.com.br>
2023-03-21 08:58:29 -0300

    inclusao do corretor e apolice

---
+++ apolice.py
@@ -0,0 +1,10 @@
+class Apolice:
+    def __init__(self, numero, tipo, valor_beneficio, segurado, corretor, data_inicio_vigencia, data_fim_vigencia, status):
+        self.numero = numero
+        self.tipo = tipo
+        self.valor_beneficio = valor_beneficio
+        self.segurado = segurado
+        self.corretor = corretor
+        self.data_inicio_vigencia = data_inicio_vigencia
+        self.data_fim_vigencia = data_fim_vigencia
+        self.status = status
---
+++ corretor.py
@@ -0,0 +1,7 @@
+class Corretor:
+    def __init__(self, primeiro_nome, sobrenome, numero_susep, apolices, contato):
+        self.primeiro_nome = primeiro_nome
+        self.sobrenome = sobrenome
+        self.numero_susep = numero_susep
+        self.apolices = apolices
+        self.contato = contato
