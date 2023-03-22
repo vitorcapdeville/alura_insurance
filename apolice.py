49e7d28a103935dbddfb1775490b85bade9c39c2
Vitor Capdeville <vgsilva@mag.com.br>
2023-03-22 09:25:55 -0300

    inclusão do uso de enum

--- apolice.py
+++ apolice.py
@@ -1,14 +1,29 @@
+from enum import Enum
+
+
+class TipoApolice(Enum):
+    VIDA = 'VIDA'
+
+
+class StatusApolice(Enum):
+    ATIVA = 'ATIVA'
+    LIQUIDADA = 'LIQUIDADA'
+    CANCELADA = 'CANCELADA'
+    EM_SINISTRO = 'EM_SINISTRO'
+
+
 class Apolice:
-    def __init__(self, numero, tipo, valor_beneficio, segurado, corretor, data_inicio_vigencia, data_fim_vigencia,
-                 status):
+    def __init__(
+        self, numero, tipo, valor_beneficio, segurado, corretor, data_inicio_vigencia, data_fim_vigencia, status
+    ):
         self._numero = numero
-        self._tipo = tipo
+        self._tipo = TipoApolice(tipo)
         self._valor_beneficio = valor_beneficio
         self._segurado = segurado
         self._corretor = corretor
         self._data_inicio_vigencia = data_inicio_vigencia
         self._data_fim_vigencia = data_fim_vigencia
-        self._status = status
+        self._status = StatusApolice(status)
 
     @property
     def valor_beneficio(self):
--- beneficiario.py
+++ beneficiario.py
@@ -1,11 +1,19 @@
+from enum import Enum
+
 from contato import Contato
 from endereco import Endereco
 from pessoa import Pessoa
 
 
+class TipoBeneficiario(Enum):
+    PARENTE = 'PARENTE'
+    AMIGO = 'AMIGO'
+    OUTROS = 'OUTROS'
+
+
 class Beneficiario:
-    def __init__(self, pessoa: Pessoa, tipo, endereco: Endereco, contato: Contato):
+    def __init__(self, pessoa: Pessoa, tipo: str, endereco: Endereco, contato: Contato):
         self._pessoa = pessoa
-        self._tipo = tipo
+        self._tipo = TipoBeneficiario(tipo)
         self._endereco = endereco
         self._contato = contato
