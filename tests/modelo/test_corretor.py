257a937d1ecf07e469dab058176eae5351ed27f3
Vitor Capdeville <vgsilva@mag.com.br>
2023-04-12 14:46:46 -0300

    inclusão de testes para o corretor

---
+++ tests/modelo/test_corretor.py
@@ -0,0 +1,38 @@
+from unittest.mock import Mock
+
+import src.modelo.corretor as modulo_corretor
+from src.modelo.corretor import Corretor
+
+
+def test_corretor_from_dict():
+    data = {
+        "nome": "Monteiro Lobato",
+        "numero_susep": "15414685940386723",
+        "cpf": "123.456.789-00",
+        "rg": "12.345.678-9",
+        "apolices": [],
+        "contato": {
+            "celular": "(11)99999-9999",
+            "telefone_residencial": "(11)2222-2222",
+            "telefone_comercial": "(11)3333-3333",
+            "email": "corretor@alura.com.br"
+        }
+    }
+    corretor = Corretor.from_dict(data)
+    assert isinstance(corretor, Corretor)
+
+
+def test_comissao_total_chama_calculadora_comissao_calcula(monkeypatch):
+    mock_calculadora_comissao = Mock()
+    monkeypatch.setattr(modulo_corretor, "CalculadoraComissao", mock_calculadora_comissao)
+    corretor = Corretor(
+        "Monteiro",
+        "Lobato",
+        "123.456.789-00",
+        "12.345.678-9",
+        Mock(),
+        "15414685940386723",
+        [],
+    )
+    corretor.comissao_total()
+    mock_calculadora_comissao.assert_called_once()
